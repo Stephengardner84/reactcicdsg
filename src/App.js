0df8a5cef137681c2ef8fcad829d0b75e197fcb5
Stephengardner84 <stephengardner84@hotmail.co.uk>
2022-08-19 13:21:15 +0100

    new changes to app.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  cons newstring = "archie";
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          change
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
